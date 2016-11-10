32c8942398c49c21024462420414c5501d53aa3d
Akash B <akash@carwow.co.uk>
2016-11-10 16:28:53 +0000

    removed app folder

--- src/controllers/base_controller.cr
+++ src/controllers/base_controller.cr
@@ -3,7 +3,7 @@ class BaseController
   macro render_template(template_name, layout = "layout")
     {{view_dir = @type.stringify.downcase.gsub(/controller/, "")}}
 
-    render "app/src/views/#{{{view_dir}}}/#{{{template_name}}}.ecr", "app/src/views/layouts/#{{{layout}}}.ecr"
+    render "src/views/#{{{view_dir}}}/#{{{template_name}}}.ecr", "src/views/layouts/#{{{layout}}}.ecr"
   end
 
 end
