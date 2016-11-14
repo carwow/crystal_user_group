38862d0a482b22dfa7787e4dc758ca33a415ddea
Akash B <akash@carwow.co.uk>
2016-11-14 09:40:58 +0000

    templating configuration files
    So that we can then start to use environment variables to set
    the properties

--- src/clug.cr
+++ src/clug.cr
@@ -4,6 +4,7 @@ require "option_parser"
 require "kemal"
 require "yaml"
 require "pg"
+require "ecr/macros"
 
 
 ENV["PORT"] = "3333" unless ENV.has_key?("PORT")
@@ -12,6 +13,16 @@ Kemal.config.port = ENV["PORT"].to_i
 
 # database
 # db = YAML.parse(File.read("./config/development.yml"))["database"]
+
+# class AppProperties
+#   ECR.def_to_s "./config/development.yml.ecr"
+# end
+
+
+io = MemoryIO.new
+ECR.embed "./config/development.yml.ecr", io
+puts io.to_s
+
 DB = PG.connect("postgres://clug:carwow@localhost/lcrug_development")                
 
 # controllers
