243fba2c5d9767226438f141a5051b3a9aaca40e
Akash B <akash@carwow.co.uk>
2016-12-02 11:25:18 +0000

    updated first event details

--- db/migrate/0002_seed_first_meetup.sql
+++ db/migrate/0002_seed_first_meetup.sql
@@ -1,1 +1,1 @@
-INSERT INTO meetups VALUES ('Inagural LCrUG Meetup', 'please attend', to_date('2017-01-14', 'YYYY-MM-DD'), null)
+INSERT INTO meetups VALUES ('Inagural LCrUG Meetup', 'https://www.meetup.com/London-Crystal-User-Group/events/235981059/', to_date('2017-01-17', 'YYYY-MM-DD'), null)
